2d749e399cfb6d21379b6e8947891a6c8222d998
ruchi5134 <kn043613@gmail.com>
2026-08-14 11:58:20 +0530

    Update OWNER and LOGGER_GROUP_ID values

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ AUTH_CHANNEL = int(getenv("AUTH_CHANNEL", None)) # Fsub Channel Id
 FSUB = getenv("FSUB", True) # Promote Bot Admin on Your Channel (Fsub Channel Id Channel ) 
 
 OWNER_ID = int(getenv("OWNER_ID", None))
-OWNER = 7473021518
-LOGGER_GROUP_ID = -1003673268434 # Bot Events Logs
+OWNER = 6467939523
+LOGGER_GROUP_ID = -1003189355460# Bot Events Logs
 
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "PURVI_BOTS")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "PURVI_UPDATES")
